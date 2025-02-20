f98a40001d7f19ca83d673bc1260e8b8e813c62f
Jan <jas.gol@poczta.onet.pl>
2025-02-20 14:25:38 +0100

    create template

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+
+const container = document.querySelector(".container");
+const text = document.createElement("p");
+text.textContent = "Hello World!";
+container.appendChild(text);
